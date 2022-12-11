b8baa9a30a18d6b48cd77eba3ff561437bec58d4
EMP2112475\markchiang <s22027761S>
2022-12-11 18:14:01 +0800

    初始化Spring boot project

---
+++ src/main/java/com/mark/springbootmall/MarktestApplication.java
@@ -0,0 +1,13 @@
+package com.mark.springbootmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarktestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarktestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mark/springbootmall/MarktestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mark.springbootmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarktestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
