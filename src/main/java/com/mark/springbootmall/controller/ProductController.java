9fca74b00414ff93b6d63cf959cb1fa826084e6f
EMP2112475\markchiang <s22027761S>
2023-01-17 23:49:41 +0800

    商品功能-實作商品列表(排序)

--- src/main/java/com/mark/springbootmall/controller/ProductController.java
+++ src/main/java/com/mark/springbootmall/controller/ProductController.java
@@ -22,13 +22,19 @@ public class ProductController {
 
     @GetMapping("/products")
     public ResponseEntity<List<Product>> getProducts(
+            // 查詢條件 Filtering
             @RequestParam(required = false) ProductCategory category,
-            @RequestParam(required = false) String search
-            ){
+            @RequestParam(required = false) String search,
 
+            // 排序 Sorting
+            @RequestParam(defaultValue = "created_date") String order,
+            @RequestParam(defaultValue = "desc") String sort
+            ){
         ProductQueryParams productQueryParams = new ProductQueryParams();
         productQueryParams.setCategory(category);
         productQueryParams.setSearch(search);
+        productQueryParams.setOrderBy(order);
+        productQueryParams.setSort(sort);
 
         List<Product> productList = productService.getProducts(productQueryParams);
         return ResponseEntity.status(HttpStatus.OK).body(productList);
